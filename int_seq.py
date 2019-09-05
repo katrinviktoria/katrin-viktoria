b86295588446346b240255a4484a7dcf0fa226d8
katrinviktoria <54946591+katrinviktoria@users.noreply.github.com>
2019-09-05 14:07:18 +0000

    Add files via upload

---
+++ int_seq.py
@@ -0,0 +1,24 @@
+num = int(input("Enter an integer: "))
+total = 0
+even = 0
+odd = 0
+biggest = 0
+
+if num > 0:
+    while num > 0:
+        if num > biggest:
+            biggest = num
+        total = total + num
+        print("Comulative total: ", total)
+        if num % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+        num = int(input("Enter an integer: "))
+
+    print("Largest number: ", biggest)
+    print("Count of even numbers: ", even)
+    print("Count of odd numbers: ", odd)
+    
+    
+    
